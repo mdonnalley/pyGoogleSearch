795a1df959e75271cc3a2ef26961e7d48f705b71
mdonnalley <michael.donnalley@gmail.com>
2015-09-14 13:55:52 -0400

    Updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyGoogleSearch',
-    version='1.0',
+    version='1.0.1',
     packages=['pyGoogleSearch'],
     url='https://github.com/mdonnalley/pyGoogleSearch',
     license='',
